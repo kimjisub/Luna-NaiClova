79c81413563c39c8b7ebefa9bdbf5b7a6d2a2fa6
김지섭 <0226daniel@gmail.com>
2018-10-27 01:09:48 +0900

    keyword 생성

---
+++ android/app/src/main/java/com/lunacoding/stellar/view/Keyword.java
@@ -0,0 +1,26 @@
+package com.lunacoding.stellar.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class Keyword extends RelativeLayout {
+	
+	public Keyword(Context context) {
+		super(context);
+	}
+	
+	public Keyword(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
+	public Keyword(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+	
+	public Keyword(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+	}
+	
+	
+}
