47a3cf17da6503a8da3ad4485134f915a0ea40d7
Kimjisub <0226daniel@gmail.com>
2020-12-06 00:20:56 +0900

    feat: new version

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -1,55 +1,63 @@
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
-const express = require('express')
-const cors = require('cors')
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
 
-admin.initializeApp(functions.config().firebase)
-admin.auth()
+admin.initializeApp(functions.config().firebase);
+admin.auth();
 const firebase = admin.database();
 const firestore = admin.firestore();
 
-const sendResponseMsg = express()
-sendResponseMsg.use(cors())
+const sendResponseMsg = express();
+sendResponseMsg.use(cors());
 sendResponseMsg.post('/:command', (req, res) => {
-	var ret = {}
+	const { command } = req.params;
+	const { msg } = req.body;
 
-	var command = req.params.command													;ret['command'] = command
-	var msg = req.body.msg
+	const ret = {
+		command,
+	};
 
-	var done = false
-	firestore.collection('command')
+	let done = false;
+	firestore
+		.collection('command')
 		.where('command', '==', command)
-		.onSnapshot(docSnapshot => {
-			if (done) return
-			done = true
+		.get()
+		.then(async (snapshot) => {
+			try {
+				if (snapshot.empty) {
+					return res.status(404).json({ err: 'Command not found' });
+				}
 
-			try{
-				docSnapshot.docChanges().forEach(change => {
-					var data = change.doc.data()
-					var id = change.doc.id												;ret['id'] = id
-					data.response = msg													;ret['response'] = msg
-					data.responseTimestamp = new Date()									;ret['responseTimestamp'] = data.responseTimestamp
-					firestore.collection('command').doc(id).set(data)
-				})
-			} catch(err){
-				ret['err'] = err.message
+				const doc = snapshot.docs[0];
+				console.log(doc.id, doc.data());
+
+				const newDoc = await firestore
+					.collection('command')
+					.doc(doc.id)
+					.update({ response: msg, responseTimestamp: new Date() });
+
+				return res.status(200).json({ id: doc.id, doc: newDoc });
+			} catch (err) {
+				console.error(err);
+				return res.status(400).json({ err: err.message });
 			}
-
-			res.status(200).send(JSON.stringify(ret))
-		}, err => {
-			res.status(400).send(err)
+		})
+		.catch((err) => {
+			console.error(err);
+			return res.status(400).json({ err: err.message });
 		});
-})
-exports.sendResponseMsg = functions.https.onRequest(sendResponseMsg)
+});
+exports.sendResponseMsg = functions.https.onRequest(sendResponseMsg);
 
-
-const addInQueue = express()
-addInQueue.use(cors())
+const addInQueue = express();
+addInQueue.use(cors());
 addInQueue.get('/:command', (req, res) => {
-	var ret = {}
-	var command = req.params.command													;ret['command'] = command
-	firebase.ref('/queue').push().set(command)
+	const ret = {};
+	const command = req.params.command;
+	ret['command'] = command;
+	firebase.ref('/queue').push().set(command);
 
-	res.status(200).send(JSON.stringify(ret))
-})
-exports.addInQueue = functions.https.onRequest(addInQueue)
+	res.status(200).send(JSON.stringify(ret));
+});
+exports.addInQueue = functions.https.onRequest(addInQueue);
